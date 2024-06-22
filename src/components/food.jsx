249e266941b08c16ef85464f3e4e974c2866e810
Gabe Eaton <eatong13@gmail.com>
2024-06-22 11:06:14 -0500

    add search bar and work on backend setup w/ database; adjusted css slighty

--- src/components/food.jsx
+++ src/components/food.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./food.css";
 import Navbar from "./navbar";
 
 function Food() {
+  const [search, setSearch] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`The search you entered was: ${search}`);
+  };
+
   return (
     <>
       <Navbar />
-      <div class="food-container">
+      <div className="food-container">
         <div className="main">
-          <div class = "search-container"><h3>Search a food to log</h3><input type="text"></input></div>
+          <div className="search-container">
+            <h3>Search a food to log</h3>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                autoFocus
+              ></input>
+              <button type="submit" className="search">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  class="bi bi-search"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                </svg>
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </>
