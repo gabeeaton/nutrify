f7bf1f18b865af5d8939b22fd53bf738987d985f
Gabe Eaton <eatong13@gmail.com>
2024-11-26 14:25:41 -0600

    fixed database issue

--- src/server/db.js
+++ src/server/db.js
@@ -2,19 +2,12 @@ import pg from "pg";
 const { Pool } = pg; 
 
 const pool = new Pool ({
-    user: "postgres",
-    password: "sheckwes0",
-    host: "localhost",
+    user: "gabeeaton",
+    password: "HYXBeklHKrvaqcnq8nzK05v3tW9lISk1",
+    host: "dpg-ct2vtlbtq21c73b94iag-a",
     port: 5432,
-    database: "nutrition_tracker"
+    database: "nutrition_tracker_db"
 })
 
 export default pool;
 
-// const pool = new Pool ({
-//     user: "gabeeaton",
-//     password: "HYXBeklHKrvaqcnq8nzK05v3tW9lISk1",
-//     host: "dpg-ct2vtlbtq21c73b94iag-a",
-//     port: 5432,
-//     database: "nutrition_tracker_db"
-// })
