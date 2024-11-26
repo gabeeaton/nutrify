a7c919203421b5cd0a3bcd1d705ed7d0f822f89e
Gabe Eaton <eatong13@gmail.com>
2024-11-26 13:04:09 -0600

    attempted svg fix

--- src/server/db.js
+++ src/server/db.js
@@ -10,3 +10,11 @@ const pool = new Pool ({
 })
 
 export default pool;
+
+// const pool = new Pool ({
+//     user: "gabeeaton",
+//     password: "HYXBeklHKrvaqcnq8nzK05v3tW9lISk1",
+//     host: "dpg-ct2vtlbtq21c73b94iag-a",
+//     port: 5432,
+//     database: "nutrition_tracker_db"
+// })
