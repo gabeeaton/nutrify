2b5cd45a64fab59c5407e985c511543a02888482
Gabe Eaton <eatong13@gmail.com>
2024-11-26 14:22:34 -0600

    fixed env issue

--- src/server/index.js
+++ src/server/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const client = new Client({
   connectionString: 'postgresql://gabeeaton:HYXBeklHKrvaqcnq8nzK05v3tW9lISk1@dpg-ct2vtlbtq21c73b94iag-a.virginia-postgres.render.com/nutrition_tracker_db',
   ssl: {
-    rejectUnauthorized: false // Set this to false to allow self-signed certificates if required
+    rejectUnauthorized: false
   }
 });
 
